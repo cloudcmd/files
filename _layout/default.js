c78511bda37e47c24cea91570e3dce064b63e052
coderaiser <mnemonic.enemy@gmail.com>
2014-03-20 06:12:24 -0400

    feature(default) google-pluseone: add mousemove

--- _layout/default.js
+++ _layout/default.js
@@ -1,11 +1,20 @@
 (function() {
     'use strict';
     
-  (function() {
-    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-    po.src = 'https://apis.google.com/js/plusone.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-  })();
+      (function() {
+        var el = document.getElementById('js-google-pluseone');
+        
+        if (el)
+            el.addEventListener('mousemove', function onMouseMove() {
+                var po, s;
+                
+                el.removeEventListener('mousemove', onMouseMove);
+                
+                po = document.createElement('script'); po.async = true;
+                po.src = 'https://apis.google.com/js/plusone.js';
+                s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+            });
+      })();
   
     var contactElement  = document.getElementById('js-contact'),
         olarkLoad;
